d194e6845ec9dfbf93196641c894d3f8f016bf5c
Elaine Ulsh <elaine.ulsh@bsu.edu>
2024-11-11 12:45:55 -0500

    Add run method and requirements to each subclass in CallCenter.java.

--- src/CallCenter.java
+++ src/CallCenter.java
@@ -17,13 +17,30 @@ public class CallCenter {
     private final static Queue<Customer> wait = new LinkedList<>();
     private final static Queue<Customer> serve = new LinkedList<>();
     private final static ReentrantLock lock = new ReentrantLock();
-    private final static Condition notEmpty = lock.newCondition();
+    private final static Condition waitNotEmpty = lock.newCondition();
+    private final static Condition waitNotFull = lock.newCondition();
+    private final static Condition serveNotEmpty = lock.newCondition();
+    private final static Condition serveNotFull = lock.newCondition();
 
+
+    // simulates a customer service agent who answers customer calls
+    // has unique ID
+    // - removes a customer from the serve queue
+    // - serves the customer by calling the provided serve mehtod
     public static class Agent implements Runnable {
         private final int ID;
 
         public Agent(int i) {
             ID = i; // can modify constructor
+        }
+
+        public void run() {
+            // remove customer from the serve queue
+            // calls the serve method
+            for (int i = 0; i < CUSTOMERS_PER_AGENT; i++) {
+                Customer customer = serve.remove();
+                serve(customer.ID);
+            }
         }
 
         public void serve(int customerID) {
@@ -33,13 +50,48 @@ public class CallCenter {
                 sleep(ThreadLocalRandom.current().nextInt(10, 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } finally {
-
             }
         }
     } // Agent class
 
+    // simulates the automated greeting service
+    // - removes customers from the wait queue
+    // - greets the customer by calling the provided greet method
+    // - places the customer into the serve queue
+    // - announces the customer's place in the serve queue
     public static class Greeter implements Runnable {
+        public void run() {
+            // removes the customer from the wait queue
+            // calls the greet method
+            // places the customer into the serve queue
+            // announces teh customer's place in the server queue
+
+            lock.lock();
+            for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+                try {
+                    while (wait.isEmpty()) {
+                        waitNotEmpty.await();
+                    }
+                    Customer customer = wait.remove();
+                    waitNotFull.signal();
+
+                    greet(customer.ID);
+
+                    while (serve.size() == NUMBER_OF_CUSTOMERS) {
+                        serveNotFull.await();
+                    }
+                    serve.add(customer);
+                    serveNotEmpty.signal();
+
+                    System.out.println("Customer " + customer.ID + " is waiting to be serviced.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+            }
+
         public void greet(int customerID) {
             System.out.println("Greeting customer " + customerID);
 
@@ -47,26 +99,34 @@ public class CallCenter {
                 sleep(ThreadLocalRandom.current().nextInt(10, 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } finally {
-
             }
-        }
-
-        public void run() {
-            lock.lock();
-            try {
-                while () {
-                    notEmpty.await();
-                }
-            } catch (InterruptedException e)
         }
     } // Greeter class
 
+    // simulates a customer call
+    // has unique ID
     public static class Customer implements Runnable {
         private final int ID;
 
         public Customer(int i) {
             ID = i;
+        }
+
+        public void run() {
+            // places customer into the wait queue
+            // each customer is a separate runnable task
+            lock.lock();
+            try {
+                while (wait.size() == NUMBER_OF_CUSTOMERS) {
+                    waitNotFull.await();
+                }
+                wait.add(new Customer(ID));
+                waitNotEmpty.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
         }
     } // Customer class
 
